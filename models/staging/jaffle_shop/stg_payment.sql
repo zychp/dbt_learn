5e74a82e30673bd4513315216e0e55e9aa28bdb1
pawelzych67@gmail.com <pawelzych67@gmail.com>
2024-01-31 14:59:27 +0000

    add payment_type in stg_payment

--- models/staging/jaffle_shop/stg_payment.sql
+++ models/staging/jaffle_shop/stg_payment.sql
@@ -1,15 +1,23 @@
-with payments as (
-    select
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod,
-    amount/100 as amount,
-    created as created_at,
-    status,
-    case
-      when status = 'success' then true
-      else false
-      end as is_completed_payment
-from {{source('stripe','payment')}}
-)
+with
+    payments as (
+        select
+            id as payment_id,
+            orderid as order_id,
+            paymentmethod,
+            amount / 100 as amount,
+            created as created_at,
+            status,
+            case
+                when status = 'success' then true else false
+            end as is_completed_payment,
+     
+            case
+              when paymentmethod in ('stripe'
+                                     ,'paypal'
+                                     ,'credit_card'
+                                     ,'gift_card')
+              then 'credit'
+              else 'cash' end as payment_type
+    from {{ source("stripe", "payment") }}
+    ) 
 select * from payments
