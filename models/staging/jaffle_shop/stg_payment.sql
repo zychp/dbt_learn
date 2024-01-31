5d060bb4cdb2c24eb28288866d3a146dc7c7b5a5
pawelzych67@gmail.com <pawelzych67@gmail.com>
2024-01-31 14:51:16 +0000

    add is_completed_payment true/false

--- models/staging/jaffle_shop/stg_payment.sql
+++ models/staging/jaffle_shop/stg_payment.sql
@@ -5,7 +5,11 @@ with payments as (
     paymentmethod,
     amount/100 as amount,
     created as created_at,
-    status
+    status,
+    case
+      when status = 'success' then true
+      else false
+      end as is_completed_payment
 from {{source('stripe','payment')}}
 )
 select * from payments
